afa1b2c6c267865512be308d0c0dc2d382f79eb9
mms-gianni <gianni.carafa@srf.ch>
2020-12-17 16:00:36 +0100

    Cleanup Anchore Stuff

--- runner.py
+++ runner.py
@@ -12,6 +12,7 @@ import uuid
 from cvss import CVSS2, CVSS3
 import base64
 from database import Database
+from anchore import Anchore
 
 report = {}
 
@@ -192,37 +193,6 @@ def getImages(containersList):
 
     return uniqueImagesList
 
-def submitImagesToAnchore(uniqueImagesList):
-    print('INFO: Submit images to Anchore')
-    for image in uniqueImagesList.values():
-        json.loads(subprocess.run(["anchore-cli", "--json", "image", "add", image['fulltag']], stdout=subprocess.PIPE).stdout.decode('utf-8'))
-        log.debug("Submitted Image: {}".format(image['fulltag']))
-
-def getImageDetailsList(uniqueImagesList):
-    print('INFO: Load imagedetails')
-    for imageUid, image in uniqueImagesList.items():
-        log.debug("Load Image: {}".format(uniqueImagesList[imageUid]['image']))
-        imagedetails = json.loads(subprocess.run(["anchore-cli", "--json", "image", "get", uniqueImagesList[imageUid]['image']], stdout=subprocess.PIPE).stdout.decode('utf-8'))[0]
-        
-        uniqueImagesList[imageUid] = {
-            'image_b64': image['image_b64'],
-            'uid': image['uid'],
-            'anchore_imageid': imagedetails['image_detail'][0]['imageId'],
-            'analyzed_at': imagedetails['analyzed_at'],
-            'created_at': imagedetails['created_at'],
-            'fulltag': imagedetails['image_detail'][0]['fulltag'],
-            'image_digest': imagedetails['imageDigest'],
-            'arch': imagedetails['image_content']['metadata']['arch'],
-            'distro': imagedetails['image_content']['metadata']['distro'],
-            'distro_version': imagedetails['image_content']['metadata']['distro_version'],
-            'image_size': imagedetails['image_content']['metadata']['image_size'],
-            'layer_count': imagedetails['image_content']['metadata']['layer_count'],
-            'registry': imagedetails['image_detail'][0]['registry'],
-            'repo': imagedetails['image_detail'][0]['repo'],
-            'dockerfile': imagedetails['image_detail'][0]['dockerfile']
-        }
-    return uniqueImagesList
-
 def addCredentials(image, repoCredentials):
 
     for credential, credentialData in repoCredentials.items():
@@ -375,58 +345,6 @@ def linkImagesToContainers(imagesList,containersList):
     
     return containerHasImage
 
-def getAnchoreVulnerabilities(imageDetailsList):
-    print('INFO: Load Vulnerabilities')
-    imageVulnListAnchore = {}
-    imageVulnSummaryAnchore = {}
-    for image in imageDetailsList.values():
-        log.debug("Load Vuln: {}".format(image['fulltag']))
-        vulnsum = {
-            'Critical': {
-                'severity': 0,
-                'total': 0,
-                'fixed': 0
-            },
-            'High': {
-                'severity': 1,
-                'total': 0,
-                'fixed': 0
-            },
-            'Medium': {
-                'severity': 2,
-                'total': 0,
-                'fixed': 0
-            },
-            'Low': {
-                'severity': 3,
-                'total': 0,
-                'fixed': 0
-            },
-            'Negligible': {
-                'severity': 4,
-                'total': 0,
-                'fixed': 0
-            },
-            'Unknown': {
-                'severity': 5,
-                'total': 0,
-                'fixed': 0
-            }
-        }
-
-        imageVuln = json.loads(subprocess.run(["anchore-cli", "--json", "image", "vuln", image['fulltag'], 'all'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
-        
-        for vulnerability in imageVuln['vulnerabilities']:
-            vulnsum[vulnerability['severity']]['total'] += 1
-            if vulnerability['fix'] != 'None':
-                vulnsum[vulnerability['severity']]['fixed'] += 1
-
-        image_uid = image['uid']
-        imageVulnListAnchore[image_uid] = imageVuln['vulnerabilities']
-        imageVulnSummaryAnchore[image_uid] = vulnsum
-    
-    return imageVulnListAnchore, imageVulnSummaryAnchore
-
 def createReport():
     global report
     reportUid = str(uuid.uuid4())
@@ -457,31 +375,7 @@ def getReportSummary(report):
     }
     return reportsummary
 
-def awaitAnalysis():
-    try:
-        allAnalyzed = False
-        while allAnalyzed == False:
-            time.sleep(3)
-            anchoreSyncStatus = json.loads(subprocess.run(["anchore-cli", "--json", "image", "list"], stdout=subprocess.PIPE).stdout.decode('utf-8'))
-
-            # Check if all images are analyzed
-            for status in anchoreSyncStatus:
-                if status['analysis_status'] == 'analyzing':
-                    print('INFO: waiting for image {0} to be analysed'.format(status['image_detail'][0]['fulltag']))
-                    allAnalyzed = False
-                    break
-                else: 
-                    allAnalyzed = True
-
-    except (KeyboardInterrupt, SystemExit):
-        print("ABORT: Analysis aborted. No data was saved. ")
-        sys.exit(0)
-    #except:
-    #    print("ERROR: Analysis aborted. No data was saved. ")
-    #    sys.exit(0)
-
 def run():
-
 
     report = createReport()
 
@@ -507,13 +401,15 @@ def run():
         imageTrivyVulnList = {}
 
     if (args.anchore == True):
-        submitImagesToAnchore(uniqueImagesList)
+        anchore = Anchore()
+        
+        anchore.submitImagesToAnchore(uniqueImagesList)
     
-        awaitAnalysis()
+        anchore.awaitAnalysis()
 
-        uniqueImagesList = getImageDetailsList(uniqueImagesList)
+        uniqueImagesList = anchore.getImageDetailsList(uniqueImagesList)
 
-        [imageVulnListAnchore, imageVulnSummary] = getAnchoreVulnerabilities(uniqueImagesList)
+        [imageVulnListAnchore, imageVulnSummary] = anchore.getAnchoreVulnerabilities(uniqueImagesList)
     else:
         imageVulnListAnchore = {}
 
